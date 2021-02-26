2fa4a91cc5b4a4d34dd97223e2f9f2717e711425
William Krug <william.p.krug@gmail.com>
2021-02-26 08:00:27 -0600

    flushed out the js side of Understanding.jsx

--- src/components/Understanding/Understanding.jsx
+++ src/components/Understanding/Understanding.jsx
@@ -6,10 +6,44 @@ import { useHistory } from 'react-router-dom';
 /* Import CSS */
 import './Understanding.css';
 
-function Understanding() {
+function Understanding({ verbose }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  /* Declare state variables */
+  const [understanding, setUnderstanding] = useState(0);
+
+  /* Helper functions */
+  const recordUnderstanding = (event) => {
+    // Keep page from reloading
+    event.preventDefault();
+
+    // Form validation
+    if (understanding === 0 || understanding === undefined) {
+      alert('*** Please enter a number between 1 and 5 and try again. ***');
+    }
+
+    // Breadcrumbs for testing and debugging
+    if (verbose) {
+      console.log('*** in recordUnderstanding() ***');
+      console.log('\tnderstanding:', understanding);
+    }
+
+    // Save value in the Redux store
+    dispatch({
+      type: 'ADD_UNDERSTANDING',
+      payload: understanding,
+    });
+
+    // Clear input
+    setUnderstanding(0);
+
+    // Navigate to next question
+    history.push('/question3');
+  };
   return (
     <div className="question">
-      <form onSubmit={recordFeelings}>
+      <form onSubmit={recordUnderstanding}>
         <h2>How well ar you understanding the content?</h2>
         <div className="left-align">
           <p>Enter a number from 1 to 5</p>
@@ -19,8 +53,8 @@ function Understanding() {
         <div className="left-align">
           <input
             type="number"
-            value={feelings}
-            onChange={(event) => setFeelings(event.target.value)}
+            value={understanding}
+            onChange={(event) => setUnderstanding(event.target.value)}
             min="1"
             max="5"
             required
