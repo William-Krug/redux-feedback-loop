270e1e128f1bb068a3b688147adbd4012ebaa63d
William Krug <william.p.krug@gmail.com>
2021-02-25 20:48:07 -0600

    created SQL db and added drop table to db scripts

--- data.sql
+++ data.sql
@@ -1,18 +1,23 @@
 -- Database should be prime_feedback
 CREATE DATABASE "prime_feedback";
 
+-- Drop Table
+DROP TABLE IF EXISTS "feedback";
+
 -- Switch to "prime_feedback" before making:
 -- Table to store the feedback
 CREATE TABLE "feedback" (
-  "id" serial primary key,
-  "feeling" INT not null,
-  "understanding" INT not null,
-  "support" INT not null,
-  "comments" text,
-  "flagged" boolean default false,
-  "date" date not null default CURRENT_DATE
+  "id" SERIAL PRIMARY KEY,
+  "feeling" INT NOT NULL,
+  "understanding" INT NOT NULL,
+  "support" INT NOT NULL,
+  "comments" TEXT,
+  "flagged" BOOLEAN DEFAULT FALSE,
+  "date" DATE NOT NULL DEFUALT CURRENT_DATE
 ); 
 
 -- Sample feedback item
-INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
-VALUES (4, 4, 5, 'Doing Great!');
+INSERT INTO "feedback"
+  ("feeling", "understanding", "support", "comments")
+VALUES
+  (4, 4, 5, 'Doing Great!');
